9b7c870ea4a9b515e3b796be6bb786d8a8fe81cc
JustKira <SHighGamerHG@Gmail.com>
2022-07-08 03:51:40 +0200

    homepage css changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,98 +5,95 @@ const Home = () => {
 
     return (
 
-        <div class="">
-            <img src={'/assets/home1.png'} alt="home1"/>
+        <div className="">
+            <img src={'/assets/home1.png'} className="cover-fill" alt="home1"/>
             <div
-                class="absolute bg-cutter bg-white h-screen w-screen -z-10 drop-shadow-white"></div>
-            <div class="absolute h-20 w-full bg-white">.</div>
+                className="absolute bg-cutter bg-white h-screen w-screen -z-10 drop-shadow-white"></div>
+            <div className="absolute h-20 w-full bg-white">.</div>
             <div
-                class="absolute bg-cutter-sec bg-white opacity-70 top-extreme-x h-screen w-screen drop-shadow-white"></div>
+                className="absolute bg-cutter-sec bg-white opacity-70 top-extreme-x h-screen w-screen drop-shadow-white"></div>
             <div
-                class="flex flex-col w-full justify-center items-center relative drop-shadow-cont">
-                <div class="mx-40">
-                    <div class="h-60 bg-white"></div>
-                    <div class="flex w-full justify-center relative">
+                className="flex flex-col w-full justify-center items-center relative drop-shadow-cont">
+                <div className="mx-40">
+                    <div className="h-60 bg-white"></div>
+                    <div className="flex w-full justify-center relative">
                         <h1
-                            class="absolute font-bold left-16 leading-snug text-hero text-white drop-shadow-white mt-10">
+                            className="absolute font-bold left-16 leading-snug text-hero-ratio text-white drop-shadow-white mt-10">
                             CUSTO-<br/>MIZE<br/>YOUR LAPTOP
                         </h1>
                         <img
-                            class="absolute drop-shadow-white bottom-120 left-44"
-                            height="200"
-                            width="200"
+                            className="absolute drop-shadow-white bottom-1/3 left-44 w-1/6"
                             src={'/assets/home3-white.png'}
                             alt=""/>
                         <img
-                            class="mix-blend-difference bg-cover overflow-visible"
+                            className="mix-blend-difference bg-cover overflow-visible w-oversize-1"
                             src={ '/assets/home6-1.png'}
                             alt=""/>
 
                         <img
-                            width="700"
-                            hight="700"
-                            class="absolute top-20 right-8 border-8 z-20 border-black"
+                            
+                            className="absolute top-20 right-8 border-8 z-20 border-black w-im-1"
                             src={ '/assets/home2.png' }
                             alt=""/>
 
                         <h1
-                            class="absolute text-3xl bottom-1/3 right-24 z-20 font-bold text-white leading-snug tracking-widest">
+                            className="absolute text-3xl bottom-1/3 right-24 hidden z-20 font-bold text-white leading-snug tracking-widest xl:block">
                             Whatever
-                            <span class="text-cyan-300">Y</span>our
-                            <span class="text-cyan-300">Laptop</span>
+                            <span className="text-cyan-300">Y</span>our
+                            <span className="text-cyan-300">Laptop</span>
                             <br/>
                             Whatever the
-                            <span class="text-cyan-300">DESIGN Y</span>ou choose
+                            <span className="text-cyan-300">DESIGN Y</span>ou choose
                         </h1>
 
                         <button
-                            class="absolute bottom-80 right-24 z-10 tracking-widest font-semibold text-white border-4 px-16 py-4 border-white rounded-md uppercase drop-shadow-white">
+                            className="absolute bottom-1/4 right-24 z-10 tracking-widest font-semibold text-white border-4 px-16 py-4 border-white rounded-md uppercase drop-shadow-white">
                             order now
                         </button>
                     </div>
                     <div>
-                        <div class="flex flex-col w-full justify-center bg-white mb-40">
-                            <div class="relative mx-20">
+                        <div className="flex flex-col w-full justify-center bg-white mb-40">
+                            <div className="relative mx-20">
                                 <img
-                                    class="absolute top-1/2 left-1/4"
+                                    className="absolute top-1/2 left-1/4"
                                     src={ '/assets/home4-2.png'}
                                     alt=""/>
-                                <h1 class="absolute font-bold left-1/3 text-hero uppercase">
+                                <h1 className="absolute font-bold left-1/3 text-hero-ratio uppercase">
                                     customize<br/>
-                                    <span class="text-cyan-300">YOUR</span>
+                                    <span className="text-cyan-300">YOUR</span>
                                     console
                                 </h1>
-                                <img class="" src={'/assets/home4-1.png'} alt=""/>
+                                <img className="" src={'/assets/home4-1.png'} alt=""/>
                                 <button
-                                    class="absolute bottom-72 right-40 z-10 tracking-widest font-semibold text-white border-4 px-16 py-4 bg-black border-black rounded-md uppercase drop-shadow-black">
+                                    className="absolute bottom-72 right-40 z-10 tracking-widest font-semibold text-white border-4 px-16 py-4 bg-black border-black rounded-md uppercase drop-shadow-black">
                                     order now
                                 </button>
                             </div>
-                            <div class="h-80"></div>
-                            <h1 class="text-hero font-bold mx-20 z-10 drop-shadow-black">
+                            <div className="h-80"></div>
+                            <h1 className="text-hero font-bold mx-20 z-10 drop-shadow-black">
                                 GAMING EVENTS
                                 <br/>
                                 & TOURNAMENTS
                             </h1>
 
-                            <div class="h-os-flat"></div>
-                            <div class="h-os-flat"></div>
+                            <div className="h-os-flat"></div>
+                            <div className="h-os-flat"></div>
                         </div>
                         <button
-                            class="absolute bottom-100 right-52 z-10 text-white border-4 px-16 py-4 tracking-widest font-semibold bg-cyan-400 border-cyan-400 rounded-md uppercase drop-shadow-black">
+                            className="absolute bottom-100 right-52 z-10 text-white border-4 px-16 py-4 tracking-widest font-semibold bg-cyan-400 border-cyan-400 rounded-md uppercase drop-shadow-black">
                             Know more
                         </button>
                     </div>
                 </div>
 
                 <img
-                    class="absolute bottom-20 overflow-visible"
+                    className="absolute bottom-20 overflow-visible"
                     src={'/assets/home5.png'}
                     alt=""/>
             </div>
 
             <img
-                class="fixed top-0 -z-50"
+                className="fixed top-0 -z-50"
                 src={'/assets/background.png' }
                 alt=""/>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
     extend: {
         fontSize: {
-            'hero': '7rem'
+            'hero': '5rem',
+            'hero-ratio': '5.5vw'
         },
         inset: {
             '100': '26rem',
@@ -23,9 +40,11 @@ module.exports = {
             'cont': ['0 0px 7px rgba(0, 0, 0, 0)', '0 0px 25px rgba(0, 0, 0, 0)']
         },
         spacing: {
-            "oversize-1": "110%",
+            "oversize-1": "115%",
+            "oversize-2": "130%",
             "os-flat":"30rem",
-            'fill-all': '200rem'
+            'fill-all': '200rem',
+            'im-1':'45%'
         },
         colors: {
             'inverted-cyan' : '#ff1a00'
